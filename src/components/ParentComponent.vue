8d63f2720a053030f395f95b5d574fa903430c11
LeonAndres888 <AL1148@live.mdx.ac.uk>
2024-03-10 23:23:34 +0000

    asd

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -148,7 +148,7 @@ export default {
 
             // Update the PUT request URL to use the correct `item.id`
             const response = await fetch(
-              `https://storefinal-env.eba-vfsgptpf.us-east-1.elasticbeanstalk.com/api/lessons/${item.id}`,
+              `https://storefinal-env.eba-vfsgptpf.us-east-1.elasticbeanstalk.com/api/lessons/${lesson.id}`,
               {
                 method: "PUT",
                 headers: { "Content-Type": "application/json" },
