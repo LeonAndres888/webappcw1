ae82d44a22ed24c5ff234bec85791b7541f4275f
LeonAndres888 <AL1148@live.mdx.ac.uk>
2024-03-10 14:07:26 +0000

    fix

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -11,6 +11,12 @@
         v-if="showProduct"
         :products="products"
         @add-item-to-cart="addItemCart"
+        :searchLesson="searchLesson"
+        :sortAttribute="sortAttribute"
+        :sortOrder="sortOrder"
+        @update:searchLesson="searchLesson = $event"
+        @update:sortAttribute="sortAttribute = $event"
+        @update-sort-order="sortOrder = $event"
       />
       <checkout-component
         v-else
