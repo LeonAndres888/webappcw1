b317f6a12cb1d85e9c0307024c40ef071c376002
LeonAndres888 <AL1148@live.mdx.ac.uk>
2024-03-10 23:44:41 +0000

    a

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,5 @@
-// Define the files we'd like to cache
-const CACHE_NAME = "v1";
-const urlsToCache = [
+const cacheName = "lessons-v1";
+const cacheFiles = [
   "/",
   "/index.html",
   "/styles.css",
@@ -9,70 +8,52 @@ const urlsToCache = [
   "/images/english.png",
   "/images/math.png",
   "/images/music.png",
-  // Add other URLs you want to cache
 ];
 
-// Install a service worker
-self.addEventListener("install", (event) => {
-  // Perform the install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
+self.addEventListener("install", function (e) {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log("[Service Worker] Caching all the files");
+      return cache.addAll(cacheFiles);
     })
   );
 });
 
-// Cache and return requests
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Cache hit - return the response
-      if (response) {
-        return response;
-      }
-
-      // IMPORTANT: Clone the request. A request is a stream and
-      // can only be consumed once. Since we are consuming this
-      // once by cache and once by the browser for fetch, we need
-      // to clone the response
-      const fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then((response) => {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
-        }
-
-        // IMPORTANT: Clone the response. A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have two streams.
-        const responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-      });
+// Add the fetch event handler to retrieve from the cache or fetch and cache
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      console.log("[Service Worker] Fetching resource: " + e.request.url);
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            console.log(
+              "[Service Worker] Caching new resource: " + e.request.url
+            );
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        })
+      );
     })
   );
 });
 
-// Update a service worker
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = ["v1"];
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
+// Update a service worker by removing old caches
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
+        keyList.map(function (key) {
+          if (key !== cacheName) {
+            console.log("[Service Worker] Removing old cache: " + key);
+            return caches.delete(key);
           }
         })
       );
     })
   );
+  return self.clients.claim();
 });
