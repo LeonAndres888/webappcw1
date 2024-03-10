1333fc5c014918ad1afdb7615ce56e3d0b0dae90
LeonAndres888 <AL1148@live.mdx.ac.uk>
2024-03-10 18:16:17 +0000

    cart functionality

--- src/components/CheckoutComponent.vue
+++ src/components/CheckoutComponent.vue
@@ -57,6 +57,101 @@ export default {
       });
     },
   },
+  submitOrder() {
+    // Submits user's order to the server and handles the response
+    const order = {
+      name: this.custName,
+      phoneNumber: this.custPhone,
+      items: this.cart.map((item) => ({
+        lessonId: item._id,
+        quantity: item.quantity,
+      })),
+    };
+
+    // Log order items to the console before submitting
+    console.log("Order items before submitting:", order.items);
+
+    fetch(
+      "https://storefinal-env.eba-vfsgptpf.us-east-1.elasticbeanstalk.com/api/orders",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(order),
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.orderSubmitted = true;
+        console.log("Order submitted:", data);
+        // Call updateLessonSpaces with a new parameter to indicate order submission
+        return this.updateLessonSpaces(order.items, true);
+      })
+      .then(() => {
+        // Re-fetch lessons to update the local state with new data from the server
+        this.fetchLessons();
+      })
+      .catch((error) => {
+        console.error("Error submitting order:", error);
+      });
+  },
+
+  updateLessonSpaces(orderedItems, isOrderSubmitted = false) {
+    // Updates available space in lessons after an order is submitted
+    if (!isOrderSubmitted) {
+      return Promise.resolve();
+    }
+
+    const updatePromises = orderedItems.map((item) => {
+      return fetch(
+        `https://storefinal-env.eba-vfsgptpf.us-east-1.elasticbeanstalk.com/api/lessons/${item.lessonId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ numberToDecrease: item.quantity }),
+        }
+      ).then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      });
+    });
+
+    return Promise.all(updatePromises)
+      .then(() => {
+        this.fetchLessons(); // Fetch the updated lessons data
+      })
+      .catch((error) => {
+        console.error("Error updating lessons:", error);
+      });
+  },
+  removeItemCart(item) {
+    let cartItemIndex = this.cart.findIndex(
+      (cartItem) => cartItem.id === item.id
+    );
+    if (cartItemIndex > -1) {
+      let cartItem = this.cart[cartItemIndex];
+      if (cartItem.quantity > 1) {
+        cartItem.quantity--;
+      } else {
+        this.cart.splice(cartItemIndex, 1);
+      }
+      // Increase the available inventory by 1
+      let lesson = this.products.find((lesson) => lesson.id === item.id);
+      if (lesson) {
+        lesson.availableInventory++;
+      }
+    }
+  },
 };
 </script>
 
