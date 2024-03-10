aacdd162510719bb993e409b5c04943cb6f1bcd9
LeonAndres888 <AL1148@live.mdx.ac.uk>
2024-03-10 23:27:35 +0000

    a

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,10 @@
-// Define the files we'd like to cache
-const CACHE_NAME = "v1";
+// Incrementing CACHE_VERSION will kick off the install event and force previously cached
+// resources to be cached again.
+const CACHE_VERSION = 2;
+const CURRENT_CACHES = {
+  prefetch: "prefetch-cache-v" + CACHE_VERSION,
+};
+
 const urlsToCache = [
   "/",
   "/index.html",
@@ -12,67 +17,55 @@ const urlsToCache = [
   // Add other URLs you want to cache
 ];
 
-// Install a service worker
 self.addEventListener("install", (event) => {
-  // Perform the install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Opened cache");
+    caches.open(CURRENT_CACHES.prefetch).then((cache) => {
       return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Cache and return requests
+self.addEventListener("activate", (event) => {
+  const expectedCacheNames = Object.values(CURRENT_CACHES);
+
+  // Active service worker takes control of the page as soon as it's in waiting
+  // This will remove any old versions of the cache
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!expectedCacheNames.includes(cacheName)) {
+              console.log("Deleting out of date cache:", cacheName);
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => {
+        return self.clients.claim();
+      })
+  );
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Cache hit - return the response
       if (response) {
         return response;
       }
-
-      // IMPORTANT: Clone the request. A request is a stream and
-      // can only be consumed once. Since we are consuming this
-      // once by cache and once by the browser for fetch, we need
-      // to clone the response
-      const fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then((response) => {
+      return fetch(event.request).then((response) => {
         // Check if we received a valid response
         if (!response || response.status !== 200 || response.type !== "basic") {
           return response;
         }
-
-        // IMPORTANT: Clone the response. A response is a stream
-        // and because we want the browser to consume the response
-        // as well as the cache consuming the response, we need
-        // to clone it so we have two streams.
         const responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then((cache) => {
+        caches.open(CURRENT_CACHES.prefetch).then((cache) => {
           cache.put(event.request, responseToCache);
         });
-
         return response;
       });
-    })
-  );
-});
-
-// Update a service worker
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = ["v1"];
-
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
     })
   );
 });
