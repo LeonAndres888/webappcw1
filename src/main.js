7ca41c46a2079335d54775f50136745927650704
LeonAndres888 <AL1148@live.mdx.ac.uk>
2024-03-11 13:48:41 +0000

    service worker fix 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import './registerServiceWorker'
+import "../registerServiceWorker";
 
 const app = createApp(App);
 app.mount("#app");
